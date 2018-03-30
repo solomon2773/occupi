63794c5bbaf00da799fe01364486b9b397ef028a
Solomon <solomon.tsao@yahoo.com>
2018-03-30 13:12:48 -0500

    User role updated
    Took 3 minutes

--- module/User/src/Controller/AuthController.php
+++ module/User/src/Controller/AuthController.php
@@ -152,6 +152,81 @@ class AuthController extends AbstractActionController
             'redirectUrl' => $redirectUrl
         ]);
     }
+    public function appLoginAction()
+    {
+        echo 'appLoginAction';
+        exit;
+        // Retrieve the redirect URL (if passed). We will redirect the user to this
+        // URL after successfull login.
+        $redirectUrl = (string)$this->params()->fromQuery('redirectUrl', '');
+        if (strlen($redirectUrl)>2048) {
+            throw new \Exception("Too long redirectUrl argument passed");
+        }
+
+        // Check if we do not have users in database at all. If so, create
+        // the 'Admin' user.
+        $this->userManager->createAdminUserIfNotExists();
+
+        // Create login form
+        $form = new LoginForm();
+        $form->get('redirect_url')->setValue($redirectUrl);
+
+        // Store login status.
+        $isLoginError = false;
+
+        // Check if user has submitted the form
+        if ($this->getRequest()->isPost()) {
+
+            // Fill in the form with POST data
+            $data = $this->params()->fromPost();
+
+            $form->setData($data);
+
+            // Validate form
+            if($form->isValid()) {
+
+                // Get filtered and validated data
+                $data = $form->getData();
+
+                // Perform login attempt.
+                $result = $this->authManager->login($data['email'],
+                    $data['password'], $data['remember_me']);
+
+                // Check result.
+                if ($result->getCode() == Result::SUCCESS) {
+
+                    // Get redirect URL.
+                    $redirectUrl = $this->params()->fromPost('redirect_url', '');
+
+                    if (!empty($redirectUrl)) {
+                        // The below check is to prevent possible redirect attack
+                        // (if someone tries to redirect user to another domain).
+                        $uri = new Uri($redirectUrl);
+                        if (!$uri->isValid() || $uri->getHost()!=null)
+                            throw new \Exception('Incorrect redirect URL: ' . $redirectUrl);
+                    }
+
+                    // If redirect URL is provided, redirect the user to that URL;
+                    // otherwise redirect to Home page.
+                    if(empty($redirectUrl)) {
+                        return $this->redirect()->toRoute('home');
+                    } else {
+                        $this->redirect()->toUrl($redirectUrl);
+                    }
+                } else {
+                    $isLoginError = true;
+                }
+            } else {
+                $isLoginError = true;
+            }
+        }
+
+        return new ViewModel([
+            'form' => $form,
+            'isLoginError' => $isLoginError,
+            'redirectUrl' => $redirectUrl
+        ]);
+    }
     //https://www.facebook.com/v2.10/dialog/oauth?scope=public_profile,user_friends,email&client_id=121251241872143&redirect_uri=https://www.yottatrend.com/login/facebookloginCallback
     public function facebookloginCallbackAction(){
         ///?error=access_denied&error_code=200&error_description=Permissions+error&error_reason=user_denied
