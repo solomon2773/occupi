8ef76e9c06e0a80643a1e9bbb933b69673a184f7
Solomon <solomon.tsao@yahoo.com>
2018-04-12 20:16:14 -0500

    update
    Took 1 minute

--- module/Occupi/src/Controller/ShowcurrentoccupancyController.php
+++ module/Occupi/src/Controller/ShowcurrentoccupancyController.php
@@ -41,7 +41,7 @@ class ShowcurrentoccupancyController extends AbstractActionController
     $dbname = "occupi";
     try {
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new \mysqli($servername, $username, $password, $dbname);
     // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
