1f197526f58ceb07789b21a3739bb751600d8fbf
Solomon <solomon.tsao@yahoo.com>
2018-04-13 13:05:55 -0500

    update
    Took 7 minutes

--- module/Occupi/src/Controller/ShowoccupancygraphController.php
+++ module/Occupi/src/Controller/ShowoccupancygraphController.php
@@ -12,17 +12,36 @@ use User\Entity\User;
 class ShowoccupancygraphController extends AbstractActionController
 {
 
-    /*
     private $entityManager;
-    
+    private $conn;
     /**
      * Constructor. Its purpose is to inject dependencies into the controller.
      */
-  //  public function __construct($entityManager)
-  //  {
-  //     $this->entityManager = $entityManager;
-  //  }
-   // */
+    public function __construct($entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $servername = "localhost";
+        $username = "occupi";
+        $password = "OccuPi@2018";
+        $dbname = "occupi";
+        try {
+
+            $this->conn = new \mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($this->conn->connect_error) {
+                die("Connection failed: " . $this->conn->connect_error);
+            }
+
+
+
+
+
+        }
+        catch(PDOException $e)
+        {
+            echo "DB Connection failed: " . $e->getMessage();
+        }
+    }
     /**
      * This is the default "index" action of the controller. It displays the 
      * Home page.
@@ -33,7 +52,24 @@ class ShowoccupancygraphController extends AbstractActionController
     }
 
     public function getOccupancyDataAction(){
-        echo '[[1, 3, 2, 4, 6, 9]]';
+        $sql_ctn = "SELECT `occupancy_counter`,`timestamp` FROM `sensors_history` WHERE `mac_address` = 'b8:27:eb:a7:c9:95' AND `timestamp` > '2018-02-24 10:00:02'";
+        $result_cnt = $this->conn->query($sql_ctn);
+        if ($result_cnt->num_rows > 0) {
+            while($row_cnt = $result_cnt->fetch_assoc()) {
+                $rows_cnt[]=$row_cnt;
+            }
+            //  $rows = $result->fetch_assoc();
+            // print_r($row);
+        } else {
+
+        }
+
+        // print_r($rows);
+        echo json_encode($rows_cnt);
+
+
+
+
         die();
     }
 
